0f86ed496cafc4835a268e2ae2f02479b4efb5d2
cedric e goddard <8ceg6@comcast.net>
2019-10-09 15:03:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 require 'pry'
 
 class Dog 
-  def initialize(name)
-    @name =name
+  def name = (dogs_name)
+    @name =dogs_name
   end 
   def name
     @name
   end
- def name=(new_name)
-@name = new_name
+ def breed=(breed)
+@breed =breed
 end 
 end
